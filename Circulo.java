eda6a108e8d12abec22a92691f2f5116305c5d38
lpzorzi <lpzorzi@gmail.com>
2016-10-11 15:58:29 -0300

    Adicionando classe circulo
    Classe Circulo

---
+++ Circulo.java
@@ -0,0 +1,20 @@
+public class Circulo {
+    
+    private float raio;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void setRaio(float raio){
+        this.raio = raio;
+    }
+    
+    public float getRaio(){
+        return raio;
+    }
+    
+}
